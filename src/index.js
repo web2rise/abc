d85a39c87b4b0dbce014b236ecfbda1f22b12de9
kapilgupta666 <kapil.gupta@traveltriangle.com>
2019-10-05 14:24:48 +0530

    push projwct

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
+class Header extends React.Component {
+    render() {
+        return (
+            <div className='flex space-between pl50 pr50'>
+                <div>
+                    <img className='w100' alt='aa' src='http://www.hibarnsley.com/wp-content/uploads/2017/06/dummy-logo.png' />
+                </div>
+                <div>
+                    <ul className='menu'>
+                        <li>Home</li>
+                        <li>Menu2</li>
+                        <li>Menu3</li>
+                        <li>Menu4</li>
+                        <li>Menu5</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Header />, document.getElementById('root'));
